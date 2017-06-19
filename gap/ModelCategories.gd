b2992ac82df62f6d3828da3bf97437661395003d
Kamal <ksaleh87@yahoo.com>
2017-06-19 16:49:46 +0200

    method record of model categories

--- gap/ModelCategories.gd
+++ gap/ModelCategories.gd
@@ -1,9 +1,220 @@
+##########################################################
+# ModelCategories                    Kamal Saleh
 #
-# ModelCategories: Model categories for CAP
+# Gap packages                       siegen, April 2017
 #
-# Declarations
-#
+##########################################################
 
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "ModelCategories_Example" );
+DeclareGlobalVariable( "CAP_INTERNAL_MODEL_CATEGORIES_BASIC_OPERATIONS" );
+DeclareGlobalVariable( "MODEL_CATEGORIES_METHOD_NAME_RECORD" );
+
+DeclareFamilyProperty( "IsWeakEquivalence",
+                       IsCapCategoryMorphism, "morphism" : reinstall := false );
+                       
+DeclareOperation( "AddIsWeakEquivalence",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsWeakEquivalence",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsWeakEquivalence",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsWeakEquivalence",
+                  [ IsCapCategory, IsList ] );
+
+DeclareFamilyProperty( "IsFibration",
+                       IsCapCategoryMorphism, "morphism" : reinstall := false );
+                       
+DeclareOperation( "AddIsFibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsFibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsFibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsFibration",
+                  [ IsCapCategory, IsList ] );
+
+DeclareFamilyProperty( "IsAcyclicFibration",
+                       IsCapCategoryMorphism, "morphism" : reinstall := false );
+                       
+DeclareOperation( "AddIsAcyclicFibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsAcyclicFibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsAcyclicFibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsAcyclicFibration",
+                  [ IsCapCategory, IsList ] );
+
+DeclareFamilyProperty( "IsCofibration",
+                       IsCapCategoryMorphism, "morphism" : reinstall := false );
+                       
+DeclareOperation( "AddIsCofibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsCofibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsCofibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsCofibration",
+                  [ IsCapCategory, IsList ] );
+                
+DeclareFamilyProperty( "IsAcyclicCofibration",
+                       IsCapCategoryMorphism, "morphism" : reinstall := false );
+                       
+DeclareOperation( "AddIsAcyclicCofibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsAcyclicCofibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsAcyclicCofibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsAcyclicCofibration",
+                  [ IsCapCategory, IsList ] );
+                  
+DeclareFamilyProperty( "IsFibrant",
+                       IsCapCategoryObject, "object" : reinstall := false );
+                       
+DeclareOperation( "AddIsFibrant",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsFibrant",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsFibrant",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsFibrant",
+                  [ IsCapCategory, IsList ] );
+
+DeclareFamilyProperty( "IsCofibrant",
+                       IsCapCategoryObject, "object" : reinstall := false );
+                       
+DeclareOperation( "AddIsCofibrant",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddIsCofibrant",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddIsCofibrant",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddIsCofibrant",
+                  [ IsCapCategory, IsList ] );
+
+##
+DeclareOperation( "Lifting", [ IsCapCategoryMorphism, IsCapCategoryMorphism, IsCapCategoryMorphism, IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddLifting",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddLifting",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddLifting",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddLifting",
+                  [ IsCapCategory, IsList ] );
+
+
+##
+DeclareOperation( "FactorThroughAcyclicFibration", [ IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddFactorThroughAcyclicFibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddFactorThroughAcyclicFibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddFactorThroughAcyclicFibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddFactorThroughAcyclicFibration",
+                  [ IsCapCategory, IsList ] );
+
+##
+DeclareOperation( "FactorThroughAcyclicCofibration", [ IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddFactorThroughAcyclicCofibration",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddFactorThroughAcyclicCofibration",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddFactorThroughAcyclicCofibration",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddFactorThroughAcyclicCofibration",
+                  [ IsCapCategory, IsList ] );
+
+##
+DeclareOperation( "AreLeftHomotopic", [ IsCapCategoryMorphism, IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddAreLeftHomotopic",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddAreLeftHomotopic",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddAreLeftHomotopic",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddAreLeftHomotopic",
+                  [ IsCapCategory, IsList ] );
+                  
+##
+DeclareOperation( "AreRightHomotopic", [ IsCapCategoryMorphism, IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddAreRightHomotopic",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddAreRightHomotopic",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddAreRightHomotopic",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddAreRightHomotopic",
+                  [ IsCapCategory, IsList ] );
+                 
+##
+DeclareOperation( "LeftHomotopy", [ IsCapCategoryMorphism, IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddLeftHomotopy",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddLeftHomotopy",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddLeftHomotopy",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddLeftHomotopy",
+                  [ IsCapCategory, IsList ] );
+
+##
+DeclareOperation( "RightHomotopy", [ IsCapCategoryMorphism, IsCapCategoryMorphism ] );
+
+DeclareOperation( "AddRightHomotopy",
+                  [ IsCapCategory, IsFunction ] );
+
+DeclareOperation( "AddRightHomotopy",
+                  [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddRightHomotopy",
+                  [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddRightHomotopy",
+                  [ IsCapCategory, IsList ] );
--- gap/ModelCategories.gi
+++ gap/ModelCategories.gi
@@ -1,10 +1,102 @@
+##########################################################
+# ModelCategories                    Kamal Saleh
 #
-# ModelCategories: Model categories for CAP
+# Gap packages                       siegen, April 2017
 #
-# Implementations
-#
-InstallGlobalFunction( ModelCategories_Example,
-function()
-	Print( "This is a placeholder function, replace it with your own code.\n" );
-end );
+##########################################################
 
+InstallValue( CAP_INTERNAL_MODEL_CATEGORIES_BASIC_OPERATIONS, rec( ) );
+
+InstallValue( MODEL_CATEGORIES_METHOD_NAME_RECORD, rec(
+
+## Basic Operations for Monoidal Categories
+
+  IsWeakEquivalence := rec(
+  installation_name := "IsWeakEquivalence",
+  filter_list := [ "morphism" ],
+  cache_name := "IsWeakEquivalence",
+  return_type := "bool" ),
+
+  IsFibration := rec(
+  installation_name := "IsFibration",
+  filter_list := [ "morphism" ],
+  cache_name := "IsFibration",
+  return_type := "bool" ),
+
+  IsAcyclicFibration := rec(
+  installation_name := "IsAcyclicFibration",
+  filter_list := [ "morphism" ],
+  cache_name := "IsAcyclicFibration",
+  return_type := "bool" ),
+
+  IsCofibration := rec(
+  installation_name := "IsCofibration",
+  filter_list := [ "morphism" ],
+  cache_name := "IsCofibration",
+  return_type := "bool" ),
+
+  IsAcyclicCofibration := rec(
+  installation_name := "IsAcyclicCofibration",
+  filter_list := [ "morphism" ],
+  cache_name := "IsAcyclicCofibration",
+  return_type := "bool" ),
+
+  IsFibrant := rec(
+  installation_name := "IsFibrant",
+  filter_list := [ "object" ],
+  cache_name := "IsFibrant",
+  return_type := "bool" ),
+
+  IsCofibrant := rec(
+  installation_name := "IsCofibrant",
+  filter_list := [ "object" ],
+  cache_name := "IsCofibrant",
+  return_type := "bool" ),
+
+  Lifting := rec(
+  installation_name := "Lifting",
+  filter_list := [ "morphism", "morphism", "morphism", "morphism" ],
+  cache_name := "Lifting",
+  return_type := "morphism" ),
+
+  FactorThroughAcyclicFibration := rec(
+  installation_name := "FactorThroughAcyclicFibration",
+  filter_list := [ "morphism" ],
+  cache_name := "FactorThroughAcyclicFibration",
+  return_type := [ "morphism", "morphism" ] ),
+
+  FactorThroughAcyclicCofibration := rec(
+  installation_name := "FactorThroughAcyclicCofibration",
+  filter_list := [ "morphism" ],
+  cache_name := "FactorThroughAcyclicCofibration",
+  return_type := [ "morphism", "morphism" ] ),
+  
+  AreLeftHomotopic := rec(
+  installation_name := "AreLeftHomotopic",
+  filter_list := [ "morphism", "morphism" ],
+  cache_name := "AreLeftHomotopic",
+  return_type := "bool" ),
+  
+  LeftHomotopy := rec(
+  installation_name := "LeftHomotopy",
+  filter_list := [ "morphism", "morphism" ],
+  cache_name := "LeftHomotopy",
+  return_type := "morphism" ),
+
+  AreRightHomotopic := rec(
+  installation_name := "AreRightHomotopic",
+  filter_list := [ "morphism", "morphism" ],
+  cache_name := "AreRightHomotopic",
+  return_type := "bool" ),
+  
+  RightHomotopy := rec(
+  installation_name := "RightHomotopy",
+  filter_list := [ "morphism", "morphism" ],
+  cache_name := "RightHomotopy",
+  return_type := "morphism" ),
+  
+  ) );
+
+CAP_INTERNAL_ENHANCE_NAME_RECORD( MODEL_CATEGORIES_METHOD_NAME_RECORD );
+
+CAP_INTERNAL_INSTALL_ADDS_FROM_RECORD( MODEL_CATEGORIES_METHOD_NAME_RECORD );
